cf59352ecf722df690ba952a832bcb9fa9bfbf29
Dyma <32801166+dymafr@users.noreply.github.com>
2023-12-13 17:46:19 +0100

    Update vite.config.js

--- native/vite.config.js
+++ native/vite.config.js
@@ -6,11 +6,5 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, 'src/index.html'),
-        form: resolve(__dirname, 'src/form/form.html'),
-      },
-    },
   },
 });
